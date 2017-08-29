ff5a00e851a25a50cac2cf1327ee4ed50b6a9cb7
finunoindia <30521070+finunoindia@users.noreply.github.com>
2017-08-29 19:10:57 +0530

    Update index.js
    Trying to fix the "Buy Infosys" bug

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ restService.post('/finUNO', function(req, res) {                    // Uses post
             for(var i=0 ; i < scrips.length ; i++){                   // Checks for scrip validity
                 if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1){
                     var j = (inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase());
-                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1)))
+                   // if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1)))
+                    if((inputText[j-1] === " " || j === 0) && (scripts[i].FIELD1[j + inputText.length] === " " || inputText.endsWith(scrips[i].FIELD1)))
                     scripnames = scrips[i].FIELD3;
                 }
                 if((inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1){
