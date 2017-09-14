f72b1f97a1747cd94e5193897d3b57a93d5a8baf
finunoindia <30521070+finunoindia@users.noreply.github.com>
2017-09-14 17:17:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,11 +44,19 @@ restService.post('/finUNO', function(req, res) {                    // Uses post
             inputText = inputText.replace("TRADE" , "");
           
             for(var i=0 ; i < scrips.length ; i++){                   // Checks for scrip validity
+                if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1){
+                    var j = (inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase());
+                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1))){
+                    scripnames = scrips[i].FIELD3;
+                    break;
+                    }
                 var scripwords = (scrips[i].FIELD1).split(" ");
                 for(var k = 0 ; k < (scripwords.length - 1) ; k++){
-                    if((inputText.toLowerCase()).search((scripwords[k]).toLowerCase()) !== -1)
-                        scripnames = scrips[i].FIELD3;
-                }
+                    if((inputText.toLowerCase()).search((scripwords[k]).toLowerCase()) !== -1){
+                       var j = (inputText.toLowerCase()).search((scripwords[k]).toLowerCase());
+                       if((inputText[j-1] === " " || j === 0) && (inputText[j + (scripwords[k]).length] === " " || inputText.endsWith(scripwords[k])))
+                       scripnames = scrips[i].FIELD3;
+                       }
                 /*if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1){
                     var j = (inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase());
                     if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1)))
