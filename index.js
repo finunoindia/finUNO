09dd0be473a25cda0e368863c4f6bd0dc8853d0d
finunoindia <30521070+finunoindia@users.noreply.github.com>
2017-11-02 19:09:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,15 +44,15 @@ restService.post('/finUNO', function(req, res) {                    // Uses post
             inputText = inputText.replace("TRADE" , "");
             // inputText = inputText.replace(" ", ""); // added by Anji
             // inputText = inputText.toLowerCase(); // added by Anji
-            if(!(inputText.endsWith(" ")))
-                inputText = inputText.concat(" ");
+            //if(!(inputText.endsWith(" ")))
+            //    inputText = inputText.concat(" ");
             
            
             for(var i=0 ; i < scrips.length ; i++){                        //checking for validity of scrip name
                 if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1){
                     console.log("Entered if 1");
                     var j = (inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase());
-                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1))){
+                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || (inputText.toLowerCase).endsWith((scrips[i].FIELD1.toLowerCase)))){
                     console.log("ENtered if 2");  
                     console.log(j);
                     console.log("#"+scrips[i].FIELD1+"#");  
