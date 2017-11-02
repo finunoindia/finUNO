4d40531a36ec0f68e3333bf69966a58f6115f110
finunoindia <30521070+finunoindia@users.noreply.github.com>
2017-11-02 18:31:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,10 @@ restService.post('/finUNO', function(req, res) {                    // Uses post
             for(var i=0 ; i < scrips.length ; i++){                        //checking for validity of scrip name
                 if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1){
                     var j = (inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase());
-                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1)))
+                    if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD1).length] === " " || inputText.endsWith(scrips[i].FIELD1))){
                     scripnames = scrips[i].FIELD3;
+                    console.log(scripnames);
+                    }
                 }
                 if((inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1){
                         var j = (inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase());
@@ -63,8 +65,7 @@ restService.post('/finUNO', function(req, res) {                    // Uses post
                         if((inputText[j-1] === " " || j === 0) && (inputText[j + (scrips[i].FIELD3).length] === " " || inputText.endsWith(scrips[i].FIELD3)))
                     scripnames = scrips[i].FIELD3;
                 }   
-            } 
-            console.log(scripnames);
+            }
             
             /* Anji
             for(var i=0 ; i < scrips.length ; i++){                        //checking for validity of scrip name
