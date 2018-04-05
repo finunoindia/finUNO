2a4014d0485fb14d818bc623e5c933e560410ed5
finunoindia <30521070+finunoindia@users.noreply.github.com>
2018-04-05 17:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ await client.end()
  client.connect(connectionString, function(err, client, done) {
    client.query('SELECT * FROM your_table', function(err, result) {
       done();
-      if(err) return console.error(err);
+      //if(err) return console.error(err);
       console.log(result.rows[0]);
    });
 });   
