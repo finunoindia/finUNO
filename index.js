b3ba0f52f3096dd92600dc1b5e83cf438b3820f2
finunoindia <30521070+finunoindia@users.noreply.github.com>
2018-04-27 11:34:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ restService.post('/finUNO', function(req, res) {                      // Uses po
  client.query('SELECT * FROM Equity', (err, res) => {
      scrips = JSON.stringify(res);
      scrips = JSON.parse(scrips);
-     console.log(scrips[0].field1);
+     console.log(scrips);
      client.end()
 })
     
